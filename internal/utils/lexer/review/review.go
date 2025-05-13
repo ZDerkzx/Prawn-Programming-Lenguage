ec82115da7e16ad8eecf8bfbcb0691e7d12085fe
ZDerkzx <derklevel@gmail.com>
2025-05-12 20:13:00 -0600

    Modulate Lexer

--- internal/utils/lexer/review/review.go
+++ internal/utils/lexer/review/review.go
@@ -9,3 +9,7 @@ func IsLetter(currentChar byte) bool {
 func IsDigit(currentChar byte) bool {
 	return currentChar >= '0' && currentChar <= '9'
 }
+
+func IsSymbol(currentChar byte) bool {
+	return currentChar == '=' || currentChar == '_' || currentChar == ')' || currentChar == '(' || currentChar == '+' || currentChar == '-' || currentChar == ';'
+}
